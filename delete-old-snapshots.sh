9a850cba39b3a9ca9b58f4d576c341f8614a5644
RDW <rdw-software@proton.me>
2024-10-29 04:29:19 +0100

    Add a workaround for lingering tags to the cleanup script
    When deleting releases manually, the tag may still persist locally.

--- delete-old-snapshots.sh
+++ delete-old-snapshots.sh
@@ -3,6 +3,9 @@ set -eu
 test $GH_TOKEN # Mandatory
 echo "Using GitHub token: $GH_TOKEN"
 
+# Local tags may be out of sync (just delete any offenders)
+git fetch --prune --prune-tags origin
+
 # Delete all but the latest snapshot (no point in storing them)
 OUTDATED_RELEASE_TAGS=$(git tag --sort=-creatordate | tail -n +2)
 TAG_COUNT=$(git tag --sort=-creatordate | tail -n +2 | wc -l)
